ffda9937c5192a82ede717f32809462ad37c404b
Bruno <bdorigon@hotmail.com>
2024-08-14 20:12:25 -0300

    separei os estilos em um novo componente

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,5 @@
-// import React from "react";
-import styled from "styled-components";
-
 import { NavLink } from "react-router-dom";
-
-const Container = styled.div`
-  display: flex;
-  max-width: 930px;
-  margin: 0 auto;
-  align-items: center;
-  flex-wrap: wrap;
-  padding: 10px 0 0 0;
-`;
-
-const Nav = styled.nav`
-  display: flex;
-  gap: 10px;
-  padding: 10px 0;
-  font-size: 1.4rem;
-`;
-
-const ButtonHeader = styled.button`
-  background: #eee;
-  border: none;
-  padding: 10px 20px;
-  font-size: 1.2rem;
-  border-radius: 4px;
-  font-weight: 400;
-  cursor: pointer;
-`;
+import { Container, Nav, ButtonHeader } from "./Styled";
 
 const Header = () => {
   return (
---
+++ src/components/Header/Styled.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  max-width: 930px;
+  margin: 0 auto;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 10px 0 0 0;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  gap: 10px;
+  padding: 10px 0;
+  font-size: 1.4rem;
+`;
+
+export const ButtonHeader = styled.button`
+  background: #eee;
+  border: none;
+  padding: 10px 20px;
+  font-size: 1.2rem;
+  border-radius: 4px;
+  font-weight: 400;
+  cursor: pointer;
+`;
